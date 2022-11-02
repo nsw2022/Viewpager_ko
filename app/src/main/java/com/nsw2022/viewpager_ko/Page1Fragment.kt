f12dcbbee876e457f004a49ccb15af13be19a46e
nsw <tmddn3410@naver.com>
2022-11-02 17:17:16 +0900

    test

--- app/src/main/java/com/nsw2022/viewpager_ko/Page1Fragment.kt
+++ app/src/main/java/com/nsw2022/viewpager_ko/Page1Fragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 open class Page1Fragment:Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
 
 
     override fun onCreateView(
@@ -20,5 +27,7 @@ open class Page1Fragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val tv:TextView = view.findViewById(R.id.tv)
+        tv.text="이야아아아아 배고프다아아"
     }
 }
